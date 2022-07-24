cbc3e336d739473ea733f88f68949da88028d9a2
Rossi Kamal <rossikamal@gmail.com>
2022-07-25 01:30:39 +0600

    Update CtaOne.js

--- components/CtaOne.js
+++ components/CtaOne.js
@@ -9,14 +9,13 @@ const CtaOne = () => {
           <Col md={12} lg={10}>
             <div className="download-app-inner text-center">
               <h2 className="h1">
-                Download our App Today &<br />
-                Experience Endless Possibilities.
+               Are you an urban planner and worried about your waste!
               </h2>
               <h3>
-                and get started with a free 1 month trial for your business
+               Lets start working with us
               </h3>
               <a href="#" className="btn">
-                Download App
+                SCHEDULE CALL
               </a>
             </div>
           </Col>
@@ -27,3 +26,4 @@ const CtaOne = () => {
 };
 
 export default CtaOne;
+;
